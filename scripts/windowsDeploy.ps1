0192895de50ed78272544ec0b58ccc2dddca64c4
Romanus <trueromanus@gmail.com>
2022-02-01 20:41:00 +0300

    + Added scripts for deploy application on windows

---
+++ scripts/windowsDeploy.ps1
@@ -0,0 +1,32 @@
+#################################################
+## Deploy windows ProxyEmy application
+##
+## You need create environment variable QTWINDEPLOYPATH
+## and set value to full path of windeployqt.exe
+##
+#################################################
+
+
+# Prepare files
+Remove-Item -Path ../deploy/output -Recurse -Force -ErrorAction Ignore
+New-Item -Path ../deploy -Name "output" -ItemType "directory"
+Copy-Item -Path ../deploy/releasebuild/ProxyEmy.exe -Destination ../deploy/output
+
+# Build project
+
+& $Env:QTWINDEPLOYPATH --no-translations --release --no-sql --no-xml --no-positioning --no-system-d3d-compiler --no-virtualkeyboard --no-opengl-sw --no-qmltooling --no-3dcore --no-3drenderer --no-3dquick --no-3dquickrenderer --no-3dinput --no-3danimation --no-3dextras --no-serialport --qmldir ../src ../deploy\output
+
+# Remove redundant thing
+Remove-Item -Path ../deploy/output\Qt6QuickTimeline.dll
+Remove-Item -Path ../deploy/output\Qt6QmlXmlListModel.dll
+Remove-Item -Path ../deploy/output\Qt6QmlLocalStorage.dll
+Remove-Item -Path ../deploy/output\QtQuick\Controls\Fusion -Recurse -Force
+Remove-Item -Path ../deploy/output\QtQuick\Controls\Imagine -Recurse -Force
+Remove-Item -Path ../deploy/output\QtQuick\Controls\Material -Recurse -Force
+Remove-Item -Path ../deploy/output\QtQuick\Controls\Windows -Recurse -Force
+
+# Copy dynamically linked libraries
+
+# Compress final build to zip file
+Remove-Item -Path ../deploy/release.zip -ErrorAction Ignore
+Compress-Archive -Path ../deploy/output/* -DestinationPath ../deploy/release.zip
